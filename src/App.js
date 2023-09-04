50a6066fa664efd0fabbad01d4b31b7ceed21e12
Marius Ileana <marius.ileana@gmail.com>
2023-09-04 12:30:56 +0300

    feat: base reqs covered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Callback from "./components/Callback";
+import authConfig from "./authConfig";
+import { UserManager, WebStorageStateStore } from "oidc-client-ts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const userManager = new UserManager({
+        userStore: new WebStorageStateStore({ store: window.localStorage }),
+        ...authConfig,
+    });
+
+    function authorize() {
+        userManager.signinRedirect({ state: "a2123a67ff11413fa19217a9ea0fbad5" });
+    }
+
+    function clearAuth() {
+        userManager.signoutRedirect();
+    }
+
+    const [authenticated, setAuthenticated] = useState(null);
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        userManager.getUser().then((user) => {
+            if (user) {
+                setAuthenticated(true);
+            } else {
+                setAuthenticated(false);
+            }
+        });
+    }, [userManager]);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Login auth={authenticated} handleLogin={authorize} />}
+                />
+                <Route
+                    path="/callback"
+                    element={
+                        <Callback
+                            auth={authenticated}
+                            setAuth={setAuthenticated}
+                            userInfo={userInfo}
+                            setUserInfo={setUserInfo}
+                            handleLogout={clearAuth}
+                            userManager={userManager}
+                        />
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/authConfig.js
@@ -0,0 +1,13 @@
+const authConfig = {
+    authority: 'http://localhost:8080/',     // Replace with your issuer URL.
+    client_id: '229664860240150969@fim',     // Replace with your client id.
+    redirect_uri: 'http://localhost:3000/callback',
+    response_type: 'code',
+    scope: 'openid profile email',
+    post_logout_redirect_uri: 'http://localhost:3000/',
+    userinfo_endpoint: 'http://localhost:8080/oidc/v1/userinfo', //Replace with your user-info endpoint
+    response_mode: 'query',
+    code_challenge_method: 'S256',
+};
+
+export default authConfig;
